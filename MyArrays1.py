a6961ec3b5ca36623694fa5b51a7595069d3044b
Annabelle Herschend <annabelle_herschend1@baylor.edu>
2021-10-06 17:17:04 -0500

    original arrays

---
+++ MyArrays1.py
@@ -0,0 +1,51 @@
+import numpy as np
+import random 
+
+# 2D array because it has rows and columns
+# 2 rows and 3 columns 1st row - 1,2,3, 2nd row - 4,5,6 and 3 element in each set
+arr01 = np.array([[1,2,3],
+                [4,5,6]])
+
+# 1D array
+arr02 = np.array([0.0, 0.1, 0.2, 0.4, 0.4])
+
+# default is a new row, we put end so that it just puts a space in between instead of sending to a new row
+for row in arr01:
+    print(row)
+    for col in row:
+        print(col, end=' ')
+    print()
+
+# flat disregards all columns and rows but goes through one at a time
+for i in arr01.flat:
+    print(i)
+
+# creates an array of 5 zeros, default is float that is why there is a decimal there
+arr03 = np.zeros(5)
+
+# array of 2 rows and 4 columns
+arr04 = np.ones((2,4), dtype=int)
+
+# 3 rows, 5 columns, all of the number 13
+arr05 = np.full((3,5),13)
+
+
+# create a 2 dimensional array of 5 integer elements each using the random module
+# and list comprehension
+
+a = np.array([[random.randint(1,10)for i in range(5)],[random.randint(1,10) for i in range(5)]])
+
+# numpy has its own random number generator so we don't have to use randint everytime
+
+b = np.array(np.random.randint(1,10, size=(2,5)))
+
+# arange is same as range but in numpy we call it arange to differentiate it
+arr06 = np.arange(5)
+
+arr07 = np.arange(5,10)
+
+arr08 = np.arange(10,1,-2)
+
+print()
+
+# size in debugger is the number of elements in our array
